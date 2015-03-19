ed64da1fe0000d0bf46fcaf399a737ac29705473
Nathan Sorenson <nathan.d.sorenson@gmail.com>
2015-03-19 16:36:56 -0700

    We can use jetty-runner from leiningen

--- project.clj
+++ project.clj
@@ -5,21 +5,20 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [io.pedestal/pedestal.service "0.4.0-SNAPSHOT" #_"0.3.1"]
-
-                 ;; Remove this line and uncomment one of the next lines to
-                 ;; use Tomcat or Immutant instead of Jetty:
-                 [io.pedestal/pedestal.jetty "0.3.1"]
-                 ;; [io.pedestal/pedestal.tomcat "0.3.1"]
-                 ;; [io.pedestal/pedestal.immutant "0.3.1"]
-
                  [ch.qos.logback/logback-classic "1.1.2" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/jul-to-slf4j "1.7.7"]
                  [org.slf4j/jcl-over-slf4j "1.7.7"]
                  [org.slf4j/log4j-over-slf4j "1.7.7"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
-  :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "hello-world-service.server/run-dev"]}
-                   :dependencies [[io.pedestal/pedestal.service-tools "0.3.1"]]}}
+  :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "hello-world-service.server/run-dev"]
+                             "jetty-runner" ["trampoline" "with-profile" "jetty-runner" "run"]} ; eg: lein jetty-runner target/hello-world.war
+                   :dependencies [[io.pedestal/pedestal.service-tools "0.3.1"]
+                                  [io.pedestal/pedestal.jetty "0.3.1"]]}
+             :jetty-runner {:dependencies ^:replace [[org.clojure/clojure "1.6.0"] ; Not sure why this needs to be here for just running a pure java jar? Get complaints if missing.
+                                                     [org.eclipse.jetty/jetty-runner "9.2.10.v20150310" :exclusions [org.glassfish/javax.el]] ; version range used on javax.el
+                                                     [org.glassfish/javax.el "3.0.0" :scope "test"]]
+                            :main org.eclipse.jetty.runner.Runner}}
   :main ^{:skip-aot true} hello-world-service.server
   :plugins [[ohpauleez/lein-pedestal "0.1.0-beta10"]]
   :pedestal {:server-ns hello-world-service.server
